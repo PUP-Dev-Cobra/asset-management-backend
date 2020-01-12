abc05a22448bb0db3c8c0d76fea12f55e89b23f0
abarcenas <aldrich.barcenas@gmail.com>
2020-01-12 19:24:42 +0800

    re-arrange for use in app factory methodology

--- internals/app.py
+++ internals/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from internals.config import DevelopmentConfig
+from flask_migrate import Migrate
 
-from routes.user import user
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__)
-app.config.from_object('internals.config.DevelopmentConfig')
-app.register_blueprint(user, url_prefix='/user')
 
-db = SQLAlchemy(app)
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from models import users, options, members, loans
+
+    from routes.user import user
+    app.register_blueprint(user, url_prefix='/user')
+    return app
--- main.py
+++
@@ -1,4 +0,0 @@
-from internals.app import app
-
-if __name__ == '__main__':
-  app.run(debug=True)
